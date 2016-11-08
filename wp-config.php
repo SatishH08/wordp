372430fbd25ee82901ee42a81b7570ee7c9c1842
Satish H <satish.h@target.com>
2016-11-08 18:18:59 +0530

    changes

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'target_opentext_training');
-
+//define('DB_NAME', 'target_opentext_training');
+define('DB_NAME', 'tgt_dam_opentext');
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'z013jlk');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'Target@1234');
+define('DB_PASSWORD', 'qazwsx!2013!!!');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'pgslx2145');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
